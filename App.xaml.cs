ceea4e49894138c02d4871f8d6285d4ddb521e29
artus9033 <artus9033@gmail.com>
2018-10-26 20:33:49 +0200

    Working floating layouts! Added icons as well

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,13 +5,25 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
+
+namespace Common.Helpers
+{
+    public static class WindowHelpers
+    {
+        public static Screen CurrentScreen(this Window window)
+        {
+            return Screen.FromPoint(new System.Drawing.Point((int)window.Left, (int)window.Top));
+        }
+    }
+}
 
 namespace WinMediaPie
 {
     /// <summary>
     /// Logika interakcji dla klasy App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : System.Windows.Application
     {
     }
 }
